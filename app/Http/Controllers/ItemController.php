d7ff2443a498865dff723b758568e491ac38e7c5
Djedaini Abdelhak <da_djedaini@esi.dz>
2022-08-05 16:54:25 +0300

    feat: items statistics api

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use Carbon\Carbon;
+use DB;
 use App\Serializers\ItemSerializer;
 use App\Serializers\ItemsSerializer;
 use App\Http\Requests\ItemRequest;
@@ -12,6 +14,28 @@ use App\Http\Resources\ItemResource;
 
 class ItemController extends ApiController
 {
+
+    /**
+     * Display items statistics.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function statistics(): JsonResponse
+    {
+        $total      = Item::count();
+        $avgPrice   = Item::avg('price');
+        $totalPrice = Item::whereMonth('created_at', Carbon::now()->month)->sum('price');
+        $websiteWithHighPrices = Item::selectRaw("sum(price) as total_prices, SUBSTRING_INDEX(SUBSTRING_INDEX(url, '://', -1),'/',1) as website")
+            ->groupBy('website')
+            ->orderBy('total_prices', 'desc')->first();
+
+        return $this->success([
+            'total_items'            => $total,
+            'average_price'          => $avgPrice,
+            'website_high_prices'    => $websiteWithHighPrices['website'],
+            'total_price_this_month' => $totalPrice,
+        ]);
+    }
 
     /**
      * Display a listing of the items [paginated].
@@ -51,7 +75,6 @@ class ItemController extends ApiController
         return $this->success(
             ['item' => new ItemResource($item)],
             'item created with success',
-            201
         );
     }
 
@@ -69,7 +92,6 @@ class ItemController extends ApiController
         return $this->success(
             ['item' => new ItemResource($item)],
             'item updated with success',
-            200
         );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('items/statistics', [ItemController::class, 'statistics']);
 Route::apiResource('items', ItemController::class)->except(['destroy']);
